b2736db3d2306074175df3bf3b5e84415b2da8b0
fravian99 <fravian99@hotmail.es>
2024-06-19 21:38:19 +0200

    App: Cambios de docker y los entornos. #36

---
+++ app/backend/app/config.py
@@ -0,0 +1,27 @@
+from functools import lru_cache
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+@lru_cache
+def get_settings():
+    return Settings()
+
+class Settings(BaseSettings):
+    PRODUCTION: bool = False
+
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: str
+
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+
+    model_config = SettingsConfigDict(
+        env_file=".env", env_ignore_empty=True, extra="ignore"
+    )
+
+settings = get_settings()
+print(settings)
--- app/backend/app/database.py
+++ app/backend/app/database.py
@@ -2,8 +2,13 @@ from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import DatabaseError
+from app.config import settings
 
-SQLALCHEMY_DATABASE = 'sqlite:///./app.db'
+SQLALCHEMY_DATABASE=""
+if settings.PRODUCTION:
+    SQLALCHEMY_DATABASE= f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_SERVER}:{settings.POSTGRES_PORT}/{settings.POSTGRES_DB}"
+else:
+    SQLALCHEMY_DATABASE = 'sqlite:///./app.db'
 
 engine: Engine = create_engine(
     SQLALCHEMY_DATABASE
