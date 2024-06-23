68725f8492d25bd95eca16145b9efd5ee6ec2ddd
fravian99 <fravian99@hotmail.es>
2024-06-23 19:16:53 +0200

    App: Pequeños cambios. #37

--- app/frontend/src/app/header/header.component.ts
+++ app/frontend/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  user: string | undefined = undefined;
+  
+  constructor(public userService: UserService, private authService: AuthService) {
+    this.user = this.userService.getUsername();
+  }
 
+  logOut() {
+    this.authService.removeToken();
+    window.location.reload();
+  }
 }
